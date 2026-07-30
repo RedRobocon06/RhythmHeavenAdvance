481f33903913179f9d9d407646adc8dc3d0e1c0c
Philblue7 <Phillipos2001@gmail.com>
2026-07-30 16:17:27 -0400

    Update files

--- data/scenes/options/data.c
+++ data/scenes/options/data.c
@@ -43,56 +43,38 @@ struct CompressedData *options_buffered_textures[] = {
     END_OF_BUFFERED_TEXTURES_LIST
 };
 
+
+// [D_08054950] Data Clear Confirmation Options
+        // Erase ALL data?
+        //　　　　　　　Erase
+        //　　　　　　　Cancel
 const char options_data_clear_confirm_text[] =
         "\0023" "\0013" "\001C" "Are you sure?\n"
         "\0021" "\0011" "\001C" "　　  　　 Yes\n"
         "　　 　 No";
 
+
+// [D_089cfb14] Option Descriptions
+    /* -------------------------------- */
+        // Sound Mode
+        // <Stereo>  For headphones, Nintendo DS, etc.
+        // <Mono>  For GBAs without headphones.
+    /* -------------------------------- */
+        // Clear Save Data
+        // Erase progress, gifts, drum lessons,
+        // <everything>! Think about it!
+    /* -------------------------------- */
+
 const char *options_desc_text[] = {
     /* SOUND MODE ------------------------------------- */
         "\0023" "\0013" "\001C" "Sound Mode\n"
-        "\0024" "\0011" "\001L" "Stereo　　" "\0021" " For dual-speaker systems. (DS, etc.)\n"
-        "\0024" "\0011" "\001L" "Mono　　 " "\x87\x53" "\0021" "For single-speaker systems. (GBA, etc.)",
+        "\0024" "\0011" "\001L" "Stereo　　" "\0021" " For those who want to use headphones.\n"
+        "\0024" "\0011" "\001L" "Mono　　 " "\x87\x53" "\0021" "For the built-in speaker of the GBA.",
     /* DATA CLEAR ------------------------------------- */
         "\0023" "\0013" "\001C" "Clear Data\n"
-        "\0021" "\0011" "\001C" "This will delete " "\0022" "all " "\0021" "of your save data and start\n"
+        "\0021" "\0011" "\001L" "This will delete " "\0022" "all " "\0021" "of your save data and start\n"
         "you over from the beginning." "\0022" " Be careful!"
     /* ------------------------------------------------ */
-};
-
-const char *advance_options_label_text[] = {
-    "Ｓｏｕｎｄ　Ｅｆｆｅｃｔｓ",
-    "Ｍｕｓｉｃ",
-#ifdef RUMBLE
-    "Ｒｕｍｂｌｅ",
-#endif
-    "Ｓｈｏｗ　Ｄｉｓｃｌａｉｍｅｒ",
-    "Ｇａｍｅ　Ｓｅｌｅｃｔ　Ｍｕｓｉｃ",
-};
-
-const char *advance_options_desc_text[] = {
-    /* NON-JP SFX ------------------------------------- */
-        "\0023" "\0013" "\001C" "Sound Effects\n"
-        "\0024" "\0011" "\001L" "English   " "\0021" "Use the localized sound effects.\n"
-        "\0024" "\0011" "\001L" "Japanese  " "\0021" "Use the original sound effects.",
-    /* NON-JP MUSIC ----------------------------------- */
-        "\0023" "\0013" "\001C" "Music\n"
-        "\0024" "\0011" "\001L" "English   " "\0021" "Use the localized music.\n"
-        "\0024" "\0011" "\001L" "Japanese  " "\0021" "Use the original music.",
-    /* RUMBLE ----------------------------------------- */
-#ifdef RUMBLE
-        "\0023" "\0013" "\001C" "Rumble\n"
-        "\0024" "\0011" "\001L" "On        " "\0021" "Rumble is active during gameplay.\n"
-        "\0024" "\0011" "\001L" "Off       " "\0021" "Rumble is disabled.",
-#endif
-    /* SHOW DISCLAIMER -------------------------------- */
-        "\0023" "\0013" "\001C" "Show Disclaimer\n"
-        "\0024" "\0011" "\001L" "Show      " "\0021" "Show the disclaimer at startup.\n"
-        "\0024" "\0011" "\001L" "Skip      " "\0021" "Skip the disclaimer at startup.",
-    /* ALT GAME SELECT MUSIC --------------------------- */
-        "\0023" "\0013" "\001C" "Game Select Music\n"
-        "\0024" "\0011" "\001L" "Normal    " "\0021" "Use Game Select 2 after the credits.\n"
-        "\0024" "\0011" "\001L" "Swapped   " "\0021" "Use Game Select 1 after the credits.",
 };
 
 
