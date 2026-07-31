3c95743b84417188281be22dae5494ece6f3de7d
Philblue7 <Phillipos2001@gmail.com>
2026-07-31 11:50:33 -0400

    Revert "Update files"
    This reverts commit 213c61dcbbfef572d20c10d801f692b3c0e302ec.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "main.h"
 #include "memory.h"
 #include "code_08003b28.h"
+#include "code_080092cc.h"
 #include "bitmap_font.h"
 #include "memory_heap.h"
+
+extern u8 haveSeenDisclaimer;
 
 
 static struct Scene *gCurrentScene;
@@ -12,7 +15,13 @@ static u8 D_03000080;
 static struct Scene *D_03000084;
 static s32 D_03000088;
 
+COMMON_DATA u8 sIsBadFlashCart = 0;
 
+#ifdef DEBUG
+#define INITIAL_SCENE &scene_debug_menu
+#else
+#define INITIAL_SCENE &scene_title
+#endif
 
 // Default Interrupt Procedure (Do Nothing)
 void interrupt_default(void) {
@@ -54,7 +63,14 @@ void func_08000224(void) {
 			flush_save_buffer_to_sram();
 		}
 	}
+#ifdef PLAYTEST
+	set_playtest_save_data();
+#endif
 	flush_save_buffer_to_sram_backup();
+
+	// Initialize disclaimer flag from save data
+	haveSeenDisclaimer = CHECK_ADVANCE_FLAG(D_030046a8->data.advanceFlags, ADVANCE_FLAG_SEEN_DISCLAIMER);
+	
 	set_sound_mode(D_030046a8->data.unk294[8]); // Set DirectSound Mode (Stereo/Mono)
 	set_scene_object_current_text_id(scene_get_default_text_id());
 	init_scene_static_var();
@@ -63,7 +79,6 @@ void func_08000224(void) {
 	func_080091d8();
 	D_03004498 = TRUE;
 }
-
 
 void agb_main(void) {
 	REG_WAITCNT = (WAITCNT_SRAM_8
@@ -89,7 +104,7 @@ void agb_main(void) {
 	REG_IME = 0;
 
 	D_03004498 = FALSE;
-
+    
 	init_ewram();
 	func_08000224();
 	debug_menu_scene_init_memory();
@@ -98,17 +113,21 @@ void agb_main(void) {
 	set_sound_mode(D_030046a8->data.unk294[8]); // Set DirectSound Mode (Stereo/Mono)
 
 	REG_DISPSTAT = 8;
-	REG_IE = (INTERRUPT_CART | INTERRUPT_DMA2 | INTERRUPT_TIMER3 | INTERRUPT_VBLANK);
+	REG_IE = (INTERRUPT_CART | INTERRUPT_DMA2 | INTERRUPT_TIMER3 | INTERRUPT_VBLANK
+	);
 	REG_IF = 0xFFFF;
 	REG_IME = 1;
 
 	func_0801d860(FALSE); // Init. Script Operator (Init. Static Variables)
 	init_scenes(&scene_warning);
-	set_scene_trans_target(&scene_warning, D_08935fac); // Title Screen
+	set_scene_trans_target(&scene_warning, (CHECK_ADVANCE_FLAG(D_030046a8->data.advanceFlags, ADVANCE_FLAG_SKIP_DISCLAIMER) ? INITIAL_SCENE : &scene_disclaimer));
+    set_scene_trans_target(&scene_disclaimer, INITIAL_SCENE);
+
 	update_key_listener();
 
 	while (TRUE) {
 		func_080013a8();
+		update_save_buffer_sram_writes();
 		get_agb_random_var();
 		update_key_listener();
 		D_030046a0 += 1;
@@ -120,7 +139,6 @@ void agb_main(void) {
 			if ((keysPressed & RESET_BUTTON_COMBO) == RESET_BUTTON_COMBO) {
 				key_rec_set_mode(0, 0x3ff, 0, 0);
 				set_current_scene(&scene_soft_reset);
-				func_08009548();
 				D_03004498 = FALSE;
 			}
 		}
@@ -337,4 +355,37 @@ void func_080006f0(struct Scene *target, s32 variable) {
 // Get Current Scene
 struct Scene *get_current_scene(void) {
 	return gCurrentScene;
+}
+
+// basically swidelay from the DS
+void delay_loop(u32 count) {
+	__asm__ volatile (
+		"1:\n"
+		"sub %0, %0, #1\n"
+		"bne 1b\n"
+		: "+r" (count)
+		:
+		: "cc", "memory"
+	);
+}
+
+void flash_check(void) {
+	const char data[] = {
+		0x77, 0x65, 0x20, 0x64, 0x6F, 0x6E, 0x74, 0x20, 0x73, 0x75, 0x70, 0x70,
+		0x6F, 0x72, 0x74, 0x20, 0x63, 0x68, 0x65, 0x61, 0x70, 0x20, 0x62, 0x6F,
+		0x6F, 0x74, 0x6C, 0x65, 0x67, 0x20, 0x20, 0x20, 0x66, 0x6C, 0x61, 0x73,
+		0x68, 0x63, 0x61, 0x72, 0x74, 0x73, 0x2C, 0x20, 0x20, 0x20, 0x20, 0x20,
+		0x74, 0x68, 0x69, 0x65, 0x66, 0x2E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	};
+
+	GBAROM[0xC0] = 33;
+	delay_loop(1000);
+
+	// really low quality flashcart - warn the user
+	if(GBAROM[0xC0] != 0) {
+		GBAROM[0xC0] = 0;
+		sIsBadFlashCart = 1;
+	}
+	
+	agb_main();
 }
