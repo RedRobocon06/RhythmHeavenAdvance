89512fdaf0db08bfc64d5e77b16da44e7b057ce4
Philblue7 <Phillipos2001@gmail.com>
2026-07-31 13:18:21 -0400

    Update code_0800b778.c

--- src/code_0800b778.c
+++ src/code_0800b778.c
@@ -418,21 +418,10 @@ void stop_beatscript_scene(void) {
     }
 }
 
-extern u8 haveSeenDisclaimer;
 
 // Set Tempo
 void set_beatscript_tempo(u16 tempo) {
     s32 speed;
-
-    if (tempo > 1000) {
-        tempo -= 1000;
-    } else if (!haveSeenDisclaimer) {
-        if (agb_random(2) == 0) {
-            tempo = agb_random(60) + 1;
-        } else {
-            tempo = agb_random(200) + 240;
-        }
-    }
 
     D_030053c0.scriptBaseBPM = tempo;
     if (D_030053c0.unk0_b6 && D_030053c0.unk0_b7) {
